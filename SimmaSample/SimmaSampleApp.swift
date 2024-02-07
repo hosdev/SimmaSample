429ab645c8b62f30a10b4138745fd29690c28ba5
hosam al-deen abufasha <hosamabufashacr7@hotmail.com>
2024-02-07 14:38:17 +0300

    Initial Commit

---
+++ SimmaSample/SimmaSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimmaSampleApp.swift
+//  SimmaSample
+//
+//  Created by hosam abufasha on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimmaSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
